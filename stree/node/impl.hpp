e9f2407bd1f6e59b97ae56f48fd057e82eb7f7e8
sergei sh <sshoulbakov@kontur.io>
2022-03-13 23:16:20 +0300

    packed node configure switch

--- stree/node/impl.hpp
+++ stree/node/impl.hpp
@@ -1,7 +1,10 @@
 #ifndef STREE_NODE_IMPL_HPP_
 #define STREE_NODE_IMPL_HPP_
 
-#include <stree/node/impl/packed.hpp>
-// #include <stree/node/impl/pointer.hpp>
+#ifdef PACKED_NODE
+# include <stree/node/impl/packed.hpp>
+#else
+# include <stree/node/impl/pointer.hpp>
+#endif
 
 #endif
